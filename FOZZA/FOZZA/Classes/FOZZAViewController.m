80ff156aea164d854ba07e286e422f117d7d4fb1
丁杰 <dingjie.fozza@bytedance.com>
2021-10-26 21:30:05 +0800

    将viewController相关的属性绑定封装到了setUp函数当中“ ；

--- FOZZA/FOZZA/Classes/FOZZAViewController.m
+++ FOZZA/FOZZA/Classes/FOZZAViewController.m
@@ -23,45 +23,32 @@
     
     // 设置普通状态下的文字属性
     NSMutableDictionary *normalAttrs = [NSMutableDictionary dictionary];
-    normalAttrs[NSFontAttributeName] = [UIFont systemFontOfSize:16];
+    normalAttrs[NSFontAttributeName] = [UIFont systemFontOfSize:14];
     [item setTitleTextAttributes:normalAttrs forState:UIControlStateNormal];
     
-    
-    UIViewController *vc0 = [[UIViewController alloc] init];
-    vc0.view.backgroundColor = [UIColor redColor];
-    vc0.tabBarItem.title = @"精华";
-    vc0.tabBarItem.image = [UIImage imageNamed:@"tabBar_essence_icon"];
-    [self addChildViewController:vc0];
-    
-    UIViewController *vc1 = [[UIViewController alloc] init];
-    vc1.view.backgroundColor = [UIColor blueColor];
-    vc1.tabBarItem.title = @"新帖";
-    vc1.tabBarItem.image = [UIImage imageNamed:@"tabBar_new_icon"];
-    [self addChildViewController:vc1];
-    
-    UIViewController *vc2 = [[UIViewController alloc] init];
-    vc2.view.backgroundColor = [UIColor grayColor];
-    vc2.tabBarItem.title = @"关注";
-    vc2.tabBarItem.image = [UIImage imageNamed:@"tabBar_follow_icon"];
-    [self addChildViewController:vc2];
-    
-    UIViewController *vc3 = [[UIViewController alloc] init];
-    vc3.view.backgroundColor = [UIColor systemPinkColor];
-    vc3.tabBarItem.title = @"我的";
-    vc3.tabBarItem.image = [UIImage imageNamed:@"tabBar_home_icon"];
-    [self addChildViewController:vc3];
+    // 添加子控制器
+    [self setupOneChildViewController:[[UITableViewController alloc] init] title:@"精华" image:@"tabBar_essence_icon" selectedImage:@"tabBar_essence_icon_clicked"];
+    [self setupOneChildViewController:[[UITableViewController alloc] init] title:@"新帖" image:@"tabBar_new_icon" selectedImage:@"tabBar_new_icon_clicked"];
+    [self setupOneChildViewController:[[UITableViewController alloc] init] title:@"关注" image:@"tabBar_follow_icon" selectedImage:@"tabBar_follow_icon_clicked"];
+    [self setupOneChildViewController:[[UITableViewController alloc] init] title:@"我的" image:@"tabBar_home_icon" selectedImage:@"tabBar_home_icon_clicked"];
     
     self.tabBar.translucent = NO;
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+/// 初始化一个子控制器
+///
+/// @param vc 子控制器类名
+/// @param title 标题
+/// @param image 图标
+/// @param selectedImage 选中的图标
+- (void)setupOneChildViewController:(UIViewController *)vc title:(NSString *)title image:(NSString *)image selectedImage:(NSString *)selectedImage
+{
+    vc.view.backgroundColor = [UIColor grayColor];
+    vc.tabBarItem.title = title;
+    vc.tabBarItem.image = [UIImage imageNamed:image];
+    vc.tabBarItem.selectedImage = [UIImage imageNamed:selectedImage];
+    [self addChildViewController:vc];
 }
-*/
 
 @end
